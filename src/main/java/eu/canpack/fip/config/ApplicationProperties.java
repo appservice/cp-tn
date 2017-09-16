fe51698a3203c56d6dd4c76848759207be59606c
Łukasz Mochel <lukasz.mochel@canpack.eu>
2017-09-16 13:52:57 +0200

    second commit

--- src/main/java/eu/canpack/fip/config/ApplicationProperties.java
+++ src/main/java/eu/canpack/fip/config/ApplicationProperties.java
@@ -9,5 +9,13 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
  */
 @ConfigurationProperties(prefix = "application", ignoreUnknownFields = false)
 public class ApplicationProperties {
+    private String drawingDirectoryPath;
 
+    public String getDrawingDirectoryPath() {
+        return drawingDirectoryPath;
+    }
+
+    public void setDrawingDirectoryPath(String drawingDirectoryPath) {
+        this.drawingDirectoryPath = drawingDirectoryPath;
+    }
 }
